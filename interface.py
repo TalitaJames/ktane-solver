b38af2035dc3dcdc1ebea4f97527aeca472c068f
TalitaJames <97495939+TalitaJames@users.noreply.github.com>
2022-06-10 22:43:27 +1000

    Restructuring of file info

--- interface.py
+++ interface.py
@@ -3,7 +3,6 @@ import sys
 
 from PyQt6.QtWidgets import QApplication
 import mainWindow 
-import bombCreatorWindow
 
 class Window:
     def __init__(self):
@@ -11,10 +10,5 @@ class Window:
         self.main_window = QtWidgets.QMainWindow()
         self.ui = mainWindow.Ui_MainWindow()
         self.ui.setupUi(self.main_window)
-
-class bombWindow:
-    def __init__(self):
-        self.app = QApplication(sys.argv)
-        self.main_window = QtWidgets.QMainWindow()
-        self.ui = bombCreatorWindow.Ui_MainWindow()
-        self.ui.setupUi(self.main_window)
+    
+    
--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-import interface
+from PyQt6 import QtWidgets
 import sys
 
+from PyQt6.QtWidgets import QApplication
+import mainWindow
+
+class Window:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.main_window = QtWidgets.QMainWindow()
+        self.ui = mainWindow.Ui_MainWindow()
+        self.ui.setupUi(self.main_window)
+        self.ui.createBomb_btn.clicked.connect(self.button_clicked)
+    
+    def button_clicked(self):
+        self.main_window.close()
+        
+    
+    
+
+
+
 if __name__ == "__main__":
-    window = interface.Window()
+    window = Window()
     window.main_window.show()
     sys.exit(window.app.exec())
 
